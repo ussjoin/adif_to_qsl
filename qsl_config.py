ca3c0a5f9788ee45dc84d8ae017674f33bc4eeac
Brendan O'Connor <brendan@maliceafterthought.com>
2022-01-29 15:18:05 -0800

    Adding config file.

---
+++ qsl_config.py
@@ -0,0 +1,12 @@
+# Yes, I should use YAML or something.
+# No, I'm not going to.
+
+# Specified in https://www.usps.com/election-mail/creating-imb-election-mail-kit.pdf
+BARCODE_ID = "00"
+
+# Assigned to a specific mailer
+MY_MAILER_ID = "000000000"
+
+# https://postalpro.usps.com/node/461
+# This means, roughly, first class, don't bother sending address correction
+SERVICE_ID = "270"
