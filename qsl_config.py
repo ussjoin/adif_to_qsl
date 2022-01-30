79cd75ec95cee224671d1d35418d4bf8476f39ac
Brendan O'Connor <brendan@maliceafterthought.com>
2022-01-30 13:13:28 -0800

    Adding printer ID to qsl_config.

--- qsl_config.py
+++ qsl_config.py
@@ -10,3 +10,7 @@ MY_MAILER_ID = "000000000"
 # https://postalpro.usps.com/node/461
 # This means, roughly, first class, don't bother sending address correction
 SERVICE_ID = "270"
+
+# For the label printer (I use a Brother QL-800)
+# Should be "usb://<vendorid>:<productid>", like "usb://0x04f9:0x209b"
+PRINTER_IDENTIFIER = "usb://0x04f9:0x209b"
